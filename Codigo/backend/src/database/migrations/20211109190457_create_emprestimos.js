72be715f268bc861e1db9c8831224ebf0487d8d7
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-11-11 16:28:01 -0300

    Alterando banco de dados

--- Codigo/backend/src/database/migrations/20211109190457_create_emprestimos.js
+++ Codigo/backend/src/database/migrations/20211109190457_create_emprestimos.js
@@ -3,8 +3,9 @@ exports.up = function (knex) {
         table.increments("id").primary();
         table.integer("idDiscente").notNullable();
         table.integer("idExemplar").notNullable();
-        table.datetime("inicio").defaultTo(knex.fn.now());
-        table.datetime("fim").nullable();
+        table.datetime("dataEmprestimo");
+        table.datetime("dataLimite");
+        table.datetime("datEntrega");
 
         // table.foreign("idDiscente").references("id").inTable("discentes");
         // table.foreign("idExemplar").references("id").inTable("exemplares");
