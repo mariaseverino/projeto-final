a362800cc426d9b4de41080e25ff037776ff18c9
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-11-14 16:06:51 -0300

    Criando tela principal

--- Codigo/frontend/src/view/Home/index.js
+++ Codigo/frontend/src/view/Home/index.js
@@ -1,5 +1,36 @@
+import { FiLogOut } from "react-icons/fi";
+import { useNavigate } from "react-router";
+
+import "../../globalStyle.css";
+import "./style.css";
+
 function Home() {
-    return <h1>Home</h1>;
+    const navigate = useNavigate();
+
+    function sair() {
+        navigate("/");
+    }
+
+    return (
+        <div id="home">
+            <div id="home-header">
+                <button id="icon-sair">
+                    <FiLogOut size={36} color="#00000" onClick={sair} />
+                </button>
+            </div>
+            <div id="home-container">
+                <div className="bloco" id="discentes">
+                    <p id="titulo-bloco">Discentes</p>
+                </div>
+                <div className="bloco" id="acervo">
+                    <p id="titulo-bloco">Acervo</p>
+                </div>
+                <div className="bloco" id="emprestimos">
+                    <p id="titulo-bloco">Emprestimos</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
