28b7f778ef43a3df05af141f110de1a22574f166
Nicolas de Oliveira <nicolasaquino13@gmail.com>
2021-11-16 18:59:44 -0300

    Atualizando arquivo de rotas

--- Codigo/frontend/src/routes.js
+++ Codigo/frontend/src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./view/Login";
 import Home from "./view/Home";
 import ListarDiscentes from "./view/ListarDiscentes";
+import CadastrarDiscente from "./view/CadastrarDiscente";
+import CadastrarExemplares from "./view/CadastrarExemplar";
+import CadastrarEmprestimo from "./view/CadastrarEmprestimo";
 
 export default function Router() {
     return (
@@ -11,6 +14,9 @@ export default function Router() {
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/discentes" element={<ListarDiscentes />} />
+                <Route path="/discentes/cadastrar" element={<CadastrarDiscente />} />
+                <Route path="/exemplar/cadastrar" element={<CadastrarExemplares />} />
+                <Route path="/emprestimo/cadastrar" element={<CadastrarEmprestimo />} />
             </Routes>
         </BrowserRouter>
     );
