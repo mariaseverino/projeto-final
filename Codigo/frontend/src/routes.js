db7095b41f48de7a79ba297e29647f4ea4e3023e
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-11-16 21:08:54 -0300

    Atualizando rotas

--- Codigo/frontend/src/routes.js
+++ Codigo/frontend/src/routes.js
@@ -4,6 +4,7 @@ import Login from "./view/Login";
 import Home from "./view/Home";
 import ListarDiscentes from "./view/ListarDiscentes";
 import ListarExemplares from "./view/ListarExemplares";
+import ListarEmprestimos from "./view/ListarEmprestimos";
 
 export default function Router() {
     return (
@@ -13,6 +14,7 @@ export default function Router() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/discentes" element={<ListarDiscentes />} />
                 <Route path="/exemplares" element={<ListarExemplares />} />
+                <Route path="/emprestimos" element={<ListarEmprestimos />} />
             </Routes>
         </BrowserRouter>
     );
