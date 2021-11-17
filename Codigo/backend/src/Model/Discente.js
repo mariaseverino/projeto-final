57a3a412c641c21293cc6f21e9197ca460aaaa6b
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-11-16 23:26:49 -0300

    Criando model Discente

---
+++ Codigo/backend/src/Model/Discente.js
@@ -0,0 +1,9 @@
+class Discente {
+    constructor(dados) {
+        this.nome = dados.nome;
+        this.matricula = dados.matricula;
+        this.cpf = dados.cpf;
+    }
+}
+
+module.exports = Discente;
