de5e7f55339e19a8033e9ba18c784d517e89944f
Nicolas de Oliveira <nicolasaquino13@gmail.com>
2021-11-21 21:26:01 -0300

    Criando opção de filtragem de discente

--- Codigo/frontend/src/view/ListarDiscentes/index.js
+++ Codigo/frontend/src/view/ListarDiscentes/index.js
@@ -12,6 +12,7 @@ import "./style.css";
 
 function ListarDiscentes() {
     const [discentes, setDiscentes] = useState([]);
+    const [busca, setBusca] = useState("");
 
     const navigate = useNavigate();
 
@@ -20,6 +21,15 @@ function ListarDiscentes() {
             setDiscentes(data);
         });
     }, []);
+
+    let discentesFiltrados = discentes.filter(
+        (discente) =>
+            discente.nome.toLowerCase().includes(busca.toLowerCase()) ||
+            discente.matricula
+                .toString()
+                .toLowerCase()
+                .includes(busca.toLowerCase())
+    );
 
     async function remover(id) {
         try {
@@ -36,41 +46,47 @@ function ListarDiscentes() {
             <Header />
             <div id="container-listar-discentes">
                 <div id="div-busca">
-                    <FiSearch size={24} />
-                    <input id="busca" />
+                    <FiSearch size={24} color="#34315e" id="icon" />
+                    <input id="busca" onChange={(e) => setBusca(e.target.value)}/>
                 </div>
                 <div>
                     <div id="titulo-discentes">
                         <h2 id="nome-discentes">Nome Aluno</h2>
                         <h2 id="matricula-discentes">Numero Matricula</h2>
                     </div>
-                    <ul>
-                        {discentes.map((dado) => (
-                            <li key={dado.id}>
-                                <div id="info-discentes">
-                                    <div id="nome-discentes">
-                                        <p>{dado.nome}</p>
+                    {discentesFiltrados.length ? (
+                        <ul>
+                            {discentesFiltrados.map((dado) => (
+                                <li key={dado.id}>
+                                    <div id="info-discentes">
+                                        <div id="nome-discentes">
+                                            <p>{dado.nome}</p>
+                                        </div>
+                                        <div id="matricula-discentes">
+                                            <p>{dado.matricula}</p>
+                                        </div>
+                                        <div id="botoes-discentes">
+                                            <Link
+                                                to={`/discente/alterar/${dado.id}`}
+                                            >
+                                                <button id="editar">Editar</button>
+                                            </Link>
+                                            <button
+                                                id="remover-discente"
+                                                onClick={() => remover(dado.id)}
+                                            >
+                                                Remover
+                                            </button>
+                                        </div>
                                     </div>
-                                    <div id="matricula-discentes">
-                                        <p>{dado.matricula}</p>
-                                    </div>
-                                    <div id="botoes-discentes">
-                                        <Link
-                                            to={`/discente/alterar/${dado.id}`}
-                                        >
-                                            <button id="editar">Editar</button>
-                                        </Link>
-                                        <button
-                                            id="remover"
-                                            onClick={() => remover(dado.id)}
-                                        >
-                                            Remover
-                                        </button>
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <div id="conteudo">
+                            <h1>Nenhum discente encontrado</h1>
+                        </div>
+                    )}
                 </div>
                 <button
                     id="adiciona"
