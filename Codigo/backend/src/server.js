343cae4f3470d6a8c0861d6b2a8e4bc103e14a81
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-11-29 19:11:37 -0300

    Atualizando servidor

--- Codigo/backend/src/server.js
+++ Codigo/backend/src/server.js
@@ -1,22 +1,5 @@
-// require("dotenv").config({
-//     path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
-// });
-// const express = require("express");
-// const cors = require("cors");
-// const routes = require("./routes");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// // app.use(routes);
-// require("dotenv").config({
-//     path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
-// });
-
 const app = require("./app");
 
 const PORT = process.env.NODE_ENV === "test" ? 3336 : 3333;
 
-console.log(PORT);
-app.listen(PORT, `running in ${PORT}`);
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
